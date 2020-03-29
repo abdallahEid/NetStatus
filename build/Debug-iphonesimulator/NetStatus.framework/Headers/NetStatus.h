b98769bc381a6fa4fce9fda6d17f458a132dc9ee
Abdallah Eid <abdallah.eid@vezeeta.com>
2020-03-29 22:06:45 +0200

    added podspec file

---
+++ build/Debug-iphonesimulator/NetStatus.framework/Headers/NetStatus.h
@@ -0,0 +1,19 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by Abdallah Eid on 3/29/20.
+//  Copyright © 2020 Abdallah Eid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
---
+++ build/NetStatus.build/Debug-iphonesimulator/NetStatus.build/DerivedSources/NetStatus_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char NetStatusVersionString[];
+ extern const double NetStatusVersionNumber;
+
+ const unsigned char NetStatusVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:NetStatus  PROJECT:NetStatus-1" "\n";
+ const double NetStatusVersionNumber __attribute__ ((used)) = (double)1.;
